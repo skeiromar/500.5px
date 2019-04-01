5c69ec05713d1b178fba9d7fd29269b18742a8ec
skeiron <omar.work2@gmail.com>
2019-04-01 13:43:03 -0400

    small comment change

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,11 @@
 #
 
 class Comment < ApplicationRecord
-    
+    validates :body, presence: true 
+
     has_many :likes, as: :likable
+    belongs_to :author, class_name: "User", foreign_key: "author_id"
+    belongs_to :picture, class_name: "Picture", foreign_key: "picture_id"
+
     
 end

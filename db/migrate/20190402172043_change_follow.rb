4084d415db8d7b1dac2b5bb97cfb5f3a81dbf10d
skeiron <omar.work2@gmail.com>
2019-04-03 16:32:52 -0400

    m

--- db/migrate/20190402172043_change_follow.rb
+++ db/migrate/20190402172043_change_follow.rb
@@ -1,5 +1,5 @@
 class ChangeFollow < ActiveRecord::Migration[5.2]
   def change
-    remove_index :follows, [:followed_id, :follower_id], unique: true
+    remove_index :follows, [:followed_id, :follower_id]
   end
 end

ddd53336d182606923870cd0e6eb20425f724999
skeiron <omar.work2@gmail.com>
2019-05-24 18:27:23 -0400

    made some changes

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -153,7 +153,9 @@ class Splash extends Component {
             <ul>
                 <li>
                     <a href="https://www.linkedin.com/in/muhammad-omar-3403a315a/">
-                        <i class="fab fa-linkedin fa-8x"></i>
+                        <abbr title="My Linkedin">
+                            <i class="fab fa-linkedin fa-8x"></i>
+                        </abbr>
                     </a>
                 </li>
             
@@ -161,7 +163,9 @@ class Splash extends Component {
                 <ul>
                 <li>
                     <a href="https://github.com/skeiromar/2048" class="github-logo-a">
-                        <i class="fab fa-github fa-8x"></i>
+                        <abbr title="My Github">
+                            <i class="fab fa-github fa-8x"></i>
+                        </abbr>
                     </a>
                 </li>
                 
@@ -169,7 +173,9 @@ class Splash extends Component {
                 <ul>
                 <li>
                     <a href="https://angel.co/muhammad-omar-5">
-                        <i class="fab fa-angellist fa-8x"></i>
+                        <abbr title="My Angel list">
+                            <i class="fab fa-angellist fa-8x"></i>
+                        </abbr>
                     </a>
                 </li>
 
@@ -177,7 +183,9 @@ class Splash extends Component {
                 <ul>
                 <li>
                     <a href="https://skeiromar.github.io/My-Portfolio-Site/">
-                        <i class="fas fa-laptop-code fa-8x"></i>
+                        <abbr title="My Personal Site">
+                            <i class="fas fa-laptop-code fa-8x"></i>
+                        </abbr>
                     </a>
                 </li>
         
